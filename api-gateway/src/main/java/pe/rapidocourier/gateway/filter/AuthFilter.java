30c7a9b6f2358c55eacfe7b78ff9b9a32d328f13
diegoalonso1208-ux <diegoalonso1208@gmail.com>
2026-06-13 11:22:29 -0500

    Proyecto final completo

---
+++ api-gateway/src/main/java/pe/rapidocourier/gateway/filter/AuthFilter.java
@@ -0,0 +1,46 @@
+package pe.rapidocourier.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    private static final String[] PUBLIC_PATHS = {
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/actuator"
+    };
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getPath().value();
+
+        for (String publicPath : PUBLIC_PATHS) {
+            if (path.startsWith(publicPath)) {
+                return chain.filter(exchange);
+            }
+        }
+
+        if (!request.getHeaders().containsKey("Authorization")) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -2;
+    }
+}
